442288c19bae51b8456a74f8d5e17b5ea5bf0b04
Christian Nagel <christiannagel@live.com>
2016-02-10 10:23:33 +0100

    delay binding

---
+++ DelayBindingSample/DelayBindingSample/SampleData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelayBindingSample
+{
+    public class SampleData
+    {
+        private ObservableCollection<string> _userInputs = new ObservableCollection<string>();
+        private string _text1;
+
+        public string Text1
+        {
+            get { return _text1; }
+            set
+            {
+                _text1 = value;
+                _userInputs.Add(_text1);
+            }
+        }
+
+        public IEnumerable<string> UserInputs => _userInputs;
+
+    }
+}
