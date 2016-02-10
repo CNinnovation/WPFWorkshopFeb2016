332464dc06af5fcaac07b9327c5b9cf81faf1f8a
Christian Nagel <christiannagel@live.com>
2016-02-10 10:52:01 +0100

    validation sample

---
+++ ValidationDemo/ValidationDemo/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ValidationDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SampleData _sampleData = new SampleData();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = _sampleData;
+        }
+
+        private void OnShowData(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(_sampleData.Val1.ToString(), _sampleData.Val2.ToString());
+        }
+    }
+}
---
+++ ValidationDemo/ValidationDemo/SampleData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationDemo
+{
+    public class SampleData : IDataErrorInfo
+    {
+        public SampleData()
+        {
+            Val1 = 5;
+            Val2 = 5;
+        }
+
+        private int _val1;
+
+        public int Val1
+        {
+            get { return _val1; }
+            set
+            {
+                if (value > 50)
+                    throw new Exception("bad bad bad value");
+                _val1 = value;
+            }
+        }
+
+        public int Val2 { get; set; }
+
+        string IDataErrorInfo.Error
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        string IDataErrorInfo.this[string columnName]
+        {
+            get
+            {
+                string errorMessage = null;
+                switch (columnName)
+                {
+                    case "Val2":
+                        if (Val2 > 40)
+                        {
+                            errorMessage = "falscher Wert bei Val2";
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                return errorMessage;
+            }
+        }
+    }
+}
