9642fd8f8b51f5040784c109eb184a7cf8fd7170
Christian Nagel <christiannagel@live.com>
2016-02-10 15:11:21 +0100

    tab control with data template selector

---
+++ TabControlSample/TabControlSample/MyContentTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TabControlSample
+{
+    public class MyContentTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+        public DataTemplate AnotherTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            MyInfo info = item as MyInfo;
+            if (info == null) return null;
+
+            if (info.Content.Contains("3"))
+            {
+                return AnotherTemplate;
+            }
+            else
+            {
+                return DefaultTemplate;
+            }
+        }
+    }
+}
---
+++ TabControlSample/TabControlSample/MyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabControlSample
+{
+    public class MyInfo
+    {
+        public string Header { get; set; }
+
+        public string Content { get; set; }
+        public override string ToString() => $"{Header} {Content}";
+    }
+}
