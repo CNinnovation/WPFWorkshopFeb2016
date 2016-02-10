919abef539c82b7a4a9ec62372d4a555978c574f
Christian Nagel <christiannagel@live.com>
2016-02-10 10:07:07 +0100

    multi binding sample

---
+++ MultiBindingSample/MultiBindingSample/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiBindingSample
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString() => $"{FirstName} {LastName}";
+
+        // string.Format("{0} {1}", FirstName, LastName);
+
+    }
+}
---
+++ MultiBindingSample/MultiBindingSample/PersonNameConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MultiBindingSample
+{
+    public class PersonNameConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            string[] names = values.Cast<string>().ToArray();
+            return $"{names[1]}, {names[0]}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
