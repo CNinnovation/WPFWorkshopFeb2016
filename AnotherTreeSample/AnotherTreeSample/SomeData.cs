8da48f5204792a54a4f48eb46acf71a25f83fbd6
Christian Nagel <christiannagel@live.com>
2016-02-11 09:42:33 +0100

    tree sample

---
+++ AnotherTreeSample/AnotherTreeSample/SomeData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherTreeSample
+{
+    public class Inner
+    {
+        public int Number { get; set; }
+
+    }
+
+    public class SomeData
+    {
+        public SomeData()
+        {
+            Inners = Enumerable.Range(0, 5).Select(x => new Inner { Number = x }).ToList();
+        }
+
+        public string Text { get; set; }
+
+        public IEnumerable<Inner> Inners { get;  }
+
+
+    }
+}
